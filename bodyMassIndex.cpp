7f0685ef9b2d1a178cafb12e7b7f9b8231d315da
Dencan <dencan.gan@gmail.com>
2020-08-15 23:20:47 +0100

    Added function to specify weight/height metric

--- bodyMassIndex.cpp
+++ bodyMassIndex.cpp
@@ -29,7 +29,61 @@ float bmiCalculator(float weight, float height) {
     return weight / pow((height / 100), 2);
 };
 
+string inputChoices(bool isWeight) {
+    
+    string metric;
+
+    if (isWeight) {
+        cout << "Please choose weight metric, enter 'kg' or 'lbs': ";
+        cin >> metric;
+        if (metric == "kg") {
+            cout << "kg selected." << endl;
+            return metric;
+        }
+        else if (metric == "lbs")
+        {
+            cout << "lbs selected." << endl;
+            return metric;
+        }
+        else {
+            cout << "Invalid weight metric defined. ";
+            return inputChoices(true);
+        }
+    }
+
+    else {
+        cout << "Please choose height metric, enter 'cm' or 'inch': ";
+        cin >> metric;
+        if (metric == "cm") {
+            cout << "cm selected. ";
+            return metric;
+        }
+        else if (metric == "inch")
+        {
+            cout << "inch selected. ";
+            return metric;
+        }
+        else {
+            cout << "Invalid height metric defined. Please enter 'cm' or 'inch'. ";
+            return inputChoices(false);
+
+        }
+    }
+}
+
 int main() {
-    float bmi = bmiCalculator(80, 180);
-    cout << bmi;
+
+    string weight = inputChoices(true);
+    string height = inputChoices(false);
+    cout << endl << weight << endl << height;
+
+    // cout << "This is a Body Mass Index (BMI) calculator" << endl;
+    // cout << "Enter your weight (kg):";
+    // cin >> weightInput;
+    // cout << "Enter your height (cm):";
+    // cin >> heightInput;
+
+    // float bmi = bmiCalculator(weightInput, heightInput);
+    // cout << "Your BMI is: " << bmi;
+
 }
